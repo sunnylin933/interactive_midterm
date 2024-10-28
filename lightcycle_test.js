24fa6704d638fbdf2693c6dd323666d3b65dff98
Sunny L. <94069216+sunnylin933@users.noreply.github.com>
2024-10-28 03:26:51 -0400

    Player 2 & Ready Checks

--- lightcycle_test.js
+++ lightcycle_test.js
@@ -1,51 +1,59 @@
+//Game Variables
 let canvasSize = 500;
+let isReady1 = false;
+let isReady2 = false;
+let isReadyChecked = false;
+let isPlaying = false;
+let isGameEnd = false;
 
 //Player1 Variables
-let player1X = canvasSize/2;
+let player1X = canvasSize / 2;
 let player1Y = canvasSize - 55;
 let player1XSpeed = 0;
 let player1YSpeed = 0;
 let player1MoveSpeed = 1.5;
-let player1Direction = 4;
+let player1Direction = 0;
 let player1Rotation = 90;
 let p1XOffset = 0;
 let p1YOffset = -20;
 
 //Player1 Lines
-let p1LineStartX = canvasSize/2;
+let p1LineStartX = canvasSize / 2;
 let p1LineStartY = canvasSize - 45;
 let p1LineEndX = 0;
 let p1LineEndY = 0;
 
 //Player2 Variables
-let player2X = canvasSize/2;
+let player2X = canvasSize / 2;
 let player2Y = 55;
 let player2XSpeed = 0;
 let player2YSpeed = 0;
 let player2MoveSpeed = 1.5;
-let player2Direction = 4;
-let player2Rotation = 90;
+let player2Direction = 2;
+let player2Rotation = 270;
 let p2XOffset = 0;
 let p2YOffset = -20;
 
 //Player2 Lines
-let p2LineStartX = canvasSize/2;
+let p2LineStartX = canvasSize / 2;
 let p2LineStartY = 55;
 let p2LineEndX = 0;
 let p2LineEndY = 0;
 
 let buffer;
 
-function preload(){
-    iPlayer1 = loadImage('images/player_char.png');
-    iPlayer2 = loadImage('images/player_char.png');
+function preload() {
+    iPlayer1 = loadImage('images/player_char_1.png');
+    iPlayer2 = loadImage('images/player_char_2.png');
 }
 
 function setup() {
     pixelDensity(1);
     createCanvas(canvasSize, canvasSize);
+
     noSmooth();
-    angleMode(DEGREES); 
+    angleMode(DEGREES);
+    textAlign(CENTER);
     background(0);
 
     buffer = createGraphics(canvasSize, canvasSize);
@@ -55,66 +63,182 @@ function setup() {
 }
 
 
-function draw(){
+function draw() {
     imageMode(CORNER);
-    background(0,0,0,60);
+    background(0, 0, 0, 60);
     image(buffer, 0, 0);
     imageMode(CENTER);
 
-    //Movement Detection - PLAYER 1
-    switch(player1Direction){
-        case 0:
-            //Forward
-            player1XSpeed = 0;
-            player1YSpeed = -1 * player1MoveSpeed;
-            player1Rotation = 90;
-            p1XOffset = 0;
-            p1YOffset = -20;
-            break;
-        case 1:
-            //Right
-            player1XSpeed = 1 * player1MoveSpeed;
-            player1YSpeed = 0;
-            player1Rotation = 180;
-            p1XOffset = 20;
-            p1YOffset = 0;
-            break;
-        case  2:
-            //Down
-            player1XSpeed = 0;
-            player1YSpeed = 1 * player1MoveSpeed;
-            player1Rotation = 270;
-            p1XOffset = 0;
-            p1YOffset = 20;
-            break;
-        case 3:
-            //Left
-            player1XSpeed = -1 * player1MoveSpeed;
-            player1YSpeed = 0;
-            player1Rotation = 0;
-            p1XOffset = -20;
-            p1YOffset = 0;
-            break;
-        case 4:
-            player1XSpeed = 0;
-            player1YSpeed = 0;
-            player1Rotation = 90;
-            p1XOffset = 0;
-            p1YOffset = -20;
+    //Game Ready Check
+    if (isReady1 && isReady2 && !isReadyChecked && !isGameEnd) {
+        setTimeout(function () {
+            isPlaying = true;
+        }, 2000);
+        isReadyChecked = true;
     }
 
-    player1X += player1XSpeed;
-    player1Y += player1YSpeed;
-
-    //P1 Line Drawing
+    //Player Code
+    //Player1 Line Drawing
     p1LineEndX = player1X;
     p1LineEndY = player1Y;
     stroke(140, 184, 255);
     strokeWeight(5);
     //draws the most recent line before it's saved
     line(p1LineStartX, p1LineStartY, p1LineEndX, p1LineEndY);
-    
-    
+
+    //Player2 Line Drawing
+    p2LineEndX = player2X;
+    p2LineEndY = player2Y;
+    stroke(252, 255, 105);
+    strokeWeight(5);
+    //draws the most recent line before it's saved
+    line(p2LineStartX, p2LineStartY, p2LineEndX, p2LineEndY);
+
+    if (isPlaying) {
+        switch (player1Direction) {
+            case 0:
+                //Forward
+                player1XSpeed = 0;
+                player1YSpeed = -1 * player1MoveSpeed;
+                player1Rotation = 90;
+                p1XOffset = 0;
+                p1YOffset = -20;
+                break;
+            case 1:
+                //Right
+                player1XSpeed = 1 * player1MoveSpeed;
+                player1YSpeed = 0;
+                player1Rotation = 180;
+                p1XOffset = 20;
+                p1YOffset = 0;
+                break;
+            case 2:
+                //Down
+                player1XSpeed = 0;
+                player1YSpeed = 1 * player1MoveSpeed;
+                player1Rotation = 270;
+                p1XOffset = 0;
+                p1YOffset = 20;
+                break;
+            case 3:
+                //Left
+                player1XSpeed = -1 * player1MoveSpeed;
+                player1YSpeed = 0;
+                player1Rotation = 0;
+                p1XOffset = -20;
+                p1YOffset = 0;
+                break;
+        }
+
+        //Movement Detection - PLAYER 2
+        switch (player2Direction) {
+            case 0:
+                //Forward
+                player2XSpeed = 0;
+                player2YSpeed = -1 * player2MoveSpeed;
+                player2Rotation = 90;
+                p2XOffset = 0;
+                p2YOffset = -20;
+                break;
+            case 1:
+                //Right
+                player2XSpeed = 1 * player2MoveSpeed;
+                player2YSpeed = 0;
+                player2Rotation = 180;
+                p2XOffset = 20;
+                p2YOffset = 0;
+                break;
+            case 2:
+                //Down
+                player2XSpeed = 0;
+                player2YSpeed = 1 * player2MoveSpeed;
+                player2Rotation = 270;
+                p2XOffset = 0;
+                p2YOffset = 20;
+                break;
+            case 3:
+                //Left
+                player2XSpeed = -1 * player2MoveSpeed;
+                player2YSpeed = 0;
+                player2Rotation = 0;
+                p2XOffset = -20;
+                p2YOffset = 0;
+                break;
+        }
+
+        player1X += player1XSpeed;
+        player1Y += player1YSpeed;
+        player2X += player2XSpeed;
+        player2Y += player2YSpeed;
+
+        //Player1 Line Drawing
+        p1LineEndX = player1X;
+        p1LineEndY = player1Y;
+        stroke(140, 184, 255);
+        strokeWeight(5);
+        //draws the most recent line before it's saved
+        line(p1LineStartX, p1LineStartY, p1LineEndX, p1LineEndY);
+
+        //Player2 Line Drawing
+        p2LineEndX = player2X;
+        p2LineEndY = player2Y;
+        stroke(252, 255, 105);
+        strokeWeight(5);
+        //draws the most recent line before it's saved
+        line(p2LineStartX, p2LineStartY, p2LineEndX, p2LineEndY);
+
+        //Player1 Collision Detection
+        let p1DetectedColor = red(buffer.get(player1X + p1XOffset, player1Y + p1YOffset));
+        //ellipse(player1X + p1XOffset, player1Y + p1YOffset, 10);
+        if (p1DetectedColor != 0) {
+            endLevel(1);
+        }
+        if (player1X + p1XOffset >= canvasSize || player1X + p1XOffset <= 0
+            || player1Y + p1YOffset >= canvasSize || player1Y + p1YOffset <= 0) {
+            endLevel(1);
+        }
+
+        //Player2 Collision Detection
+        let p2DetectedColor = red(buffer.get(player2X + p2XOffset, player2Y + p2YOffset));
+        //ellipse(player2X + p2XOffset, player2Y + p2YOffset, 10);
+        if (p2DetectedColor != 0) {
+            endLevel(2);
+        }
+        if (player2X + p2XOffset >= canvasSize || player2X + p2XOffset <= 0
+            || player2Y + p2YOffset >= canvasSize || player2Y + p2YOffset <= 0) {
+            endLevel(2);
+        }
+    }
+    else {
+        if (!isGameEnd) {
+            noStroke();
+            textSize(12);
+            if (!isReady2) {
+                fill(255);
+                text('NOT READY', canvasSize / 2, 120);
+            }
+            else {
+                fill(22, 128, 0);
+                text('READY', canvasSize / 2, 120);
+            }
+
+            if (!isReady1) {
+                fill(255);
+                text('NOT READY', canvasSize / 2, canvasSize - 110);
+            }
+            else {
+                fill(22, 128, 0);
+                text('READY', canvasSize / 2, canvasSize - 110);
+            }
+
+            if (isReady1 && isReady2 && !isPlaying) {
+                fill(255);
+                textSize(18);
+                text('GAME STARTING...', canvasSize / 2, canvasSize / 2);
+            }
+        }
+    }
+
     //Player1 Display
     push();
     translate(player1X, player1Y);
@@ -122,138 +246,118 @@ function draw(){
     image(iPlayer1, 0, 0, 48, 24);
     pop();
 
-    //Player1 Collision Detection
-    let p1DetectedColor = blue(buffer.get(player1X + p1XOffset, player1Y + p1YOffset));
-    if(p1DetectedColor != 0){
-        resetLevel();
-    }
-    if(player1X + p1XOffset >= canvasSize || player1X + p1XOffset <= 0
-        || player1Y + p1YOffset >= canvasSize || player1Y + p1YOffset <= 0){
-        resetLevel();
-    }
-    //Debug Detector
-    //circle(playerX + detectorXOffset, playerY + detectorYOffset, 5);
-
-
-    //Movement Detection - PLAYER 2
-    switch(player2Direction){
-        case 0:
-            //Forward
-            player2XSpeed = 0;
-            player2YSpeed = -1 * player2MoveSpeed;
-            player2Rotation = 90;
-            p2XOffset = 0;
-            p2YOffset = -20;
-            break;
-        case 1:
-            //Right
-            player2XSpeed = 1 * player2MoveSpeed;
-            player2YSpeed = 0;
-            player2Rotation = 180;
-            p2XOffset = 20;
-            p2YOffset = 0;
-            break;
-        case  2:
-            //Down
-            player2XSpeed = 0;
-            player2YSpeed = 1 * player2MoveSpeed;
-            player2Rotation = 270;
-            p2XOffset = 0;
-            p2YOffset = 20;
-            break;
-        case 3:
-            //Left
-            player2XSpeed = -1 * player2MoveSpeed;
-            player2YSpeed = 0;
-            player2Rotation = 0;
-            p2XOffset = -20;
-            p2YOffset = 0;
-            break;
-        case 4:
-            player2XSpeed = 0;
-            player2YSpeed = 0;
-            player2Rotation = 270;
-            p2XOffset = 0;
-            p2YOffset = -20;
-    }
-
-    player2X += player2XSpeed;
-    player2Y += player2YSpeed;
-
-    //P1 Line Drawing
-    p2LineEndX = player2X;
-    p2LineEndY = player2Y;
-    stroke(140, 184, 255);
-    strokeWeight(5);
-    //draws the most recent line before it's saved
-    line(p2LineStartX, p2LineStartY, p2LineEndX, p2LineEndY);
-    
-    
-    //Player1 Display
+    //Player2 Display
     push();
     translate(player2X, player2Y);
     rotate(player2Rotation);
     image(iPlayer2, 0, 0, 48, 24);
     pop();
+}
 
-    //Player1 Collision Detection
-    let detectedColor = blue(buffer.get(player1X + p1XOffset, player1Y + p1YOffset));
-    if(detectedColor != 0){
-        resetLevel();
+function keyPressed() {
+    if (isPlaying) {
+        //Player 1
+        if (player1YSpeed == 0) { //This prevents 180 turns
+            if (key === 'w') {
+                saveLine(1);
+                player1Direction = 0;
+
+            }
+            if (key === 's') {
+                saveLine(1);
+                player1Direction = 2;
+
+            }
+        }
+        if (player1XSpeed == 0) {
+            if (key === 'd') {
+                saveLine(1);
+                player1Direction = 1;
+
+            }
+            if (key === 'a') {
+                saveLine(1);
+                player1Direction = 3;
+
+            }
+        }
+
+        //Player 2
+        if (player2YSpeed == 0) { //This prevents 180 turns
+            if (keyCode === UP_ARROW) {
+                saveLine(2);
+                player2Direction = 0;
+
+            }
+            if (keyCode === DOWN_ARROW) {
+                saveLine(2);
+                player2Direction = 2;
+
+            }
+        }
+        if (player2XSpeed == 0) {
+            if (keyCode === RIGHT_ARROW) {
+                saveLine(2);
+                player2Direction = 1;
+
+            }
+            if (keyCode === LEFT_ARROW) {
+                saveLine(2);
+                player2Direction = 3;
+
+            }
+        }
     }
-    if(player2X + p2XOffset >= canvasSize || player2X + p2XOffset <= 0
-        || player2Y + p2YOffset >= canvasSize || player2Y + p2YOffset <= 0){
-        resetLevel();
+    else {
+        if (!isGameEnd) {
+            //Ready Checks
+            if (keyCode === UP_ARROW || keyCode === DOWN_ARROW || keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
+                isReady2 = true;
+            }
+
+            if (key === 'w' || key === 'a' || key === 's' || key === 'd') {
+                isReady1 = true;
+            }
+        }
+        else
+        {
+
+        }
     }
 }
 
-function keyPressed(){
-    if(player1YSpeed == 0){ //This prevents 180 turns
-        if(key === 'w'){
-            saveP1Line();
-            player1Direction = 0;
-            
-        }
-        if(key === 's'){
-            saveP1Line();
-            player1Direction = 2;
-            
-        }
-    }
-    if(player1XSpeed == 0){
-        if(key === 'd'){
-            saveP1Line();
-            player1Direction = 1;
-            
-        }
-        if(key === 'a'){
-            saveP1Line();
-            player1Direction = 3;
-            
-        }
-    }
-}
-
-function saveP1Line(){
+function saveLine(player) {
     console.log('Registering new line.')
-    buffer.stroke(140, 184, 255);
-    buffer.strokeWeight(5);
-    buffer.line(p1LineStartX, p1LineStartY, player1X, player1Y);
-    
-    //moves the goalposts
-    p1LineStartX = player1X;
-    p1LineStartY = player1Y;
+    if (player == 1) {
+        buffer.stroke(140, 184, 255);
+        buffer.strokeWeight(5);
+        buffer.line(p1LineStartX, p1LineStartY, player1X, player1Y);
+
+        //moves the goalposts
+        p1LineStartX = player1X;
+        p1LineStartY = player1Y;
+    }
+
+    if (player == 2) {
+        buffer.stroke(252, 255, 105);
+        buffer.strokeWeight(5);
+        buffer.line(p2LineStartX, p2LineStartY, player2X, player2Y);
+
+        //moves the goalposts
+        p2LineStartX = player2X;
+        p2LineStartY = player2Y;
+    }
 }
 
-function resetLevel(){
-    //Reset Variables
-    player1X = canvasSize/2;
-    player1Y = canvasSize - 55;
-    player1Direction = 4;
 
-    p1LineStartX = canvasSize/2;
-    p1LineStartY = canvasSize - 45;
-    buffer = createGraphics(canvasSize, canvasSize);
+function endLevel(losingPlayer) {
+    console.log('Game Ended');
+    isPlaying = false;
+    isGameEnd = true;
+
+    textSize(20);
+    fill(255);
+    //Reset Variable
 }
 
 /* OBSOLETE
